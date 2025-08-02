e6d14d1847b8c860e1d811f8b682b352e3c6d531
umang_1312 <ucbeingcoder@gmail.com>
2025-08-02 11:20:38 +0530

    loop logical error correction

--- loops&strings/Practice_QS2.js
+++ loops&strings/Practice_QS2.js
@@ -2,8 +2,9 @@ let GameNumber=25;
 let UserNo=prompt("Guess any number!!!");
 let exit=0;
 while(UserNo != GameNumber){
+    UserNo=prompt("Opps!!! wrong number try again!!");
+    if(UserNo == GameNumber) break;
     exit=prompt("If you wanna exit game press 1 else continue with 0");
     if(exit==1) break;
-    else UserNo=prompt("Opps!!! wrong number try again!!");
 }
-console.log("Congratulation!!! You Won!!")
+console.log("Congratulation!!! You Won!!");
