71e5e9afa10e9c5ced688b97ad4d77ddb0b637aa
umang_1312 <ucbeingcoder@gmail.com>
2025-08-02 20:21:16 +0530

    Create a function using the “function” keyword that takes a String as an argument & returns the number of vowels in the string

---
+++ Functions/Practice_QS1.js
@@ -0,0 +1,11 @@
+function VowelInString(str){
+    let vowel=0;
+    for(let i=0;i<=str.length;i++){
+    if(str.charAt(i)=='a' || str.charAt(i)=='e' || str.charAt(i)=='i' || str.charAt(i)=='o' || str.charAt(i)=='u' || str.charAt(i)=='A' || str.charAt(i)=='E' || str.charAt(i)=='I' || str.charAt(i)=='O' || str.charAt(i)=='U'){
+        vowel++;
+    }
+}
+return vowel;
+}
+let count=VowelInString(prompt("Enter A String"));
+console.log("Number Of Vowel : ",count);
