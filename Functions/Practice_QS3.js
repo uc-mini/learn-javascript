2641028310d7574a284fb5a0a63e626a40e6ea53
umang_1312 <ucbeingcoder@gmail.com>
2025-08-04 21:23:12 +0530

    Added Practice_QS3 files

---
+++ Functions/Practice_QS3.js
@@ -0,0 +1,10 @@
+let arr=["Pune","Delhi"];
+
+arr.forEach((val)=>{
+    console.log(val.toUpperCase());
+})
+let brr;
+let arr1=[1,2,3,4,5,6,7,8];
+arr1.forEach((val)=>{
+    console.log(val*val);
+});
